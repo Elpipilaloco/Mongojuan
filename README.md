3203583ac28adf34bbaccde99cd8b0445414c57d
Robero <robertoeschdz@gmail.com>
2022-06-30 18:11:00 -0500

    Make it better2

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+### Variables de entorno
+
+Para que funcione la app es necesario añadir las siguientes variables de entorno:
+
+MONGODB_URI
+PORT
---
+++ src/app.js
@@ -0,0 +1,30 @@
+import path from "path";
+import express from "express";
+import indexRoutes from "./routes/index";
+import { create } from "express-handlebars";
+import morgan from "morgan";
+
+const app = express();
+
+// settings
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  create({
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    defaulLayout: "main",
+    extname: ".hbs",
+  }).engine
+);
+app.set("view engine", ".hbs");
+
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended: false}))
+
+app.use(indexRoutes);
+
+app.use(express.static(path.join(__dirname, "public")))
+
+export default app;
